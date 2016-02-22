0ac7e9e723823705dd53cf79ace21dba6c946fea
Christoph Witzko <github@christophwitzko.com>
2016-02-22 15:02:03 +0100

    test: proxy to PouchDB instance

--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,7 @@
 var Hapi = require('hapi')
-var H2o2 = require('h2o2')
+var h2o2 = require('h2o2')
 var nock = require('nock')
+var request = require('request').defaults({json: true})
 var test = require('tap').test
 
 var plugin = require('../')
@@ -9,26 +10,16 @@ var noop = function () {}
 
 function provisionServer () {
   var server = new Hapi.Server()
-  server.connection()
-  server.register(H2o2, noop)
+  server.connection({port: 12345})
+  server.register(h2o2, noop)
   return server
 }
-
-test('options.couchdb is required', function (t) {
-  t.plan(2)
-
-  var server = provisionServer()
-  server.register(plugin, {}, function (error) {
-    t.ok(error instanceof TypeError, 'register fails with TypeError')
-    t.is(error.message, 'options.couchdb must be set', 'register fails with error message')
-  })
-})
 
 test('proxies request to options.couchdb', function (t) {
   t.plan(1)
 
   var mock = nock('http://example.com')
-    .get('/foo')
+    .get('/foo/')
     .reply(200, {ok: true})
 
   var server = provisionServer()
@@ -40,7 +31,35 @@ test('proxies request to options.couchdb', function (t) {
   }, noop)
 
   server.inject('/foo', function (response) {
-    t.ok(mock.isDone(), 'request proxied to http://example.com/foo')
+    t.ok(mock.isDone(), 'request proxied to http://example.com/foo/')
+  })
+})
+
+test('proxies request to options.pouchdb', function (t) {
+  t.plan(4)
+
+  var server = provisionServer()
+  server.register({
+    register: plugin,
+    options: {
+      pouchdb: {
+        db: require('memdown')
+      }
+    }
+  }, noop)
+
+  t.tearDown(server.stop.bind(server, null))
+
+  server.start(function () {
+    request.get('http://127.0.0.1:12345/foo', function (err, res, data) {
+      t.error(err, 'error')
+      t.equal(res.statusCode, 404, 'correct statusCode')
+      server.stop()
+    })
+    request.get('http://127.0.0.1:12345/', function (err, res, data) {
+      t.error(err, 'error')
+      t.equal(res.statusCode, 200, 'correct statusCode')
+    })
   })
 })
 
