6fcda266da7c36ca44e956ab00895b386c329f9e
Gregor <gregor@martynus.net>
2016-09-10 16:29:59 -0700

    fix(api): add workaround for dbnames with / & leveldb adapter
    see https://github.com/pouchdb/pouchdb/issues/5668

--- api/store/create.js
+++ api/store/create.js
@@ -1,6 +1,9 @@
 module.exports = createStore
 
+var pathResolve = require('path').resolve
+
 var _ = require('lodash')
+var mkdirp = require('mkdirp')
 
 var addRolePrivilege = require('../utils/add-role-privilege')
 var errors = require('../utils/errors')
@@ -38,12 +41,34 @@ function createStore (state, name, options) {
     return state.metaDb.put(doc)
 
     .then(function () {
-      var db = new state.PouchDB(name)
       if (!state.usesHttpAdapter) {
+        var db = new state.PouchDB(name)
         return db.info()
+
+        // TODO: remove once https://github.com/pouchdb/pouchdb/issues/5668 is resolved
+        // also remove mkdirp from package.json unless it’s used somewhere else
+        .catch(function (error) {
+          if (error.name === 'OpenError') {
+            var path = db.__opts.prefix ? db.__opts.prefix + name : name
+            return new Promise(function (resolve, reject) {
+              mkdirp(pathResolve(path), function (error) {
+                if (error) {
+                  return reject(error)
+                }
+
+                resolve()
+              })
+            })
+
+            .then(function () {
+              return new state.PouchDB(name).info()
+            })
+          }
+          throw error
+        })
       }
 
-      return db.info().then(function () {
+      return new state.PouchDB(name).info().then(function () {
         var options = {
           couchDbUrl: state.couchDbUrl,
           dbName: name
